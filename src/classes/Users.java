e522a7892e0c6b629970fcb4fdbab2144033bf98
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-07-22 16:40:05 +0200

    Style : created setUserName and get getUserName;

--- src/classes/Users.java
+++ src/classes/Users.java
@@ -1,5 +1,30 @@
 package classes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Users {
 
+  private static String userName;
+  public static List<String> userNames = new ArrayList<>();
+  private String name;
+  private boolean child;
+
+  public static void setUserName(String userName) {
+    Users.userName = userName;
+  }
+
+  public static String getUserName() {
+    return userName;
+  }
+
+  public Users() {
+
+  }
+  /**
+   * Methods read login and password from file, check it
+   *
+   * @param usersFile file with password and login
+   * @throws IOException can throw
+   */
 }
