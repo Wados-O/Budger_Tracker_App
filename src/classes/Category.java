62f76a49bef48763d7674571a9eaf10728cf4172
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-07-22 15:01:50 +0200

    feat : add Array createCategories

--- src/classes/Category.java
+++ src/classes/Category.java
@@ -1,5 +1,8 @@
 package classes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Category {
 
   private int id;
@@ -13,5 +16,29 @@ public class Category {
     this.id = id;
     this.title = title;
     this.allowed = allowed;
+  }
+  public static List<Category> categories = new ArrayList<>();
+  public static List<Category> createCategories(){
+    Category c0 = new Category(0,"Food",false);
+    categories.add(c0);
+    Category c1 = new Category(1,"Goods",true);
+    categories.add(c1);
+    Category c2 = new Category(2,"Clothes",true);
+    categories.add(c2);
+    Category c3 = new Category(3,"Entertainment",false);
+    categories.add(c3);
+    Category c4 = new Category(4,"House",true);
+    categories.add(c4);
+    Category c5 = new Category(5,"Transport",true);
+    categories.add(c5);
+    Category c6 = new Category(6,"Fuel",true);
+    categories.add(c6);
+    Category c7 = new Category(7,"Hobby",false);
+    categories.add(c7);
+    Category c8 = new Category(8,"Vacations",false);
+    categories.add(c8);
+    Category c9 = new Category(9,"Medicine",false);
+    categories.add(c9);
+    return categories;
   }
 }
