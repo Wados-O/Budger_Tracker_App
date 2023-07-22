892e1881cc63251a6cbd1d95d73b1b98281c44ef
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-07-22 20:34:51 +0200

    Style : add calcBalance to Operations ;

--- src/classes/Operations.java
+++ src/classes/Operations.java
@@ -25,4 +25,21 @@ public class Operations {
     return dateFormat.format(date);
   }
 
+
+  /**
+   * Calculate payment balance in period
+   *
+   * @param records   List of Record with payments
+   * @param dateBegin Begin of payments period minus one day
+   * @param dateEnd   End of payments period plus one day
+   * @return Sum of payments in this period
+   */
+  public static double calcBalance(List<Record> records, Date dateBegin, Date dateEnd) {
+    return records.stream()
+        .filter(x -> x.getDate().after(dateBegin))
+        .filter(x -> x.getDate().before(dateEnd))
+        .mapToDouble(Record::getAmount)
+        .sum();
+  }
+
 }
