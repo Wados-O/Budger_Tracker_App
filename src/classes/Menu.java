74efc0b001e16ae4daaa5f28512a6e34f1d06136
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-07-22 17:04:25 +0200

    Style : add printHeader to manu;

--- src/classes/Menu.java
+++ src/classes/Menu.java
@@ -1,5 +1,22 @@
 package classes;
 
 public class Menu {
+  public static final String LINEOPEN = "┌┈┈┈┈┈┈┰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┒";
+  public static final String HEADER = "│  ID  │          DATA          │         CATEGORY        │         USER         │         AMOUNT         │             COMMENTS             │";
+  public static final String LINECLOSE = "┠┈┈┈┈┈┈┼┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┼┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┼┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┼┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┼┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┤";
+  /**
+   * Print header for table of records
+   *
+   */
+
+  public static void printHeader(){
+    System.out.println(Colors.WHITE_BOLD_BRIGHT + LINEOPEN + Colors.RESET);
+    System.out.println(Colors.WHITE_BOLD_BRIGHT + HEADER + Colors.RESET);
+    System.out.println(Colors.WHITE_BOLD_BRIGHT + Colors.RESET);
+  }
+  /**
+   * make delay for 2000ms
+   *
+   */
 
 }
