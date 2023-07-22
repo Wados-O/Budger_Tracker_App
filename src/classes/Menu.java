3e67ec969f71d339b75ac67be7c37e5641a18ca6
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-07-22 17:09:03 +0200

    Style : add delaySecond to manu;

--- src/classes/Menu.java
+++ src/classes/Menu.java
@@ -1,28 +1,39 @@
 package classes;
 
 public class Menu {
+
   public static final String LINEOPEN = "┌┈┈┈┈┈┈┰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┒";
   public static final String HEADER = "│  ID  │          DATA          │         CATEGORY        │         USER         │         AMOUNT         │             COMMENTS             │";
   public static final String LINECLOSE = "┠┈┈┈┈┈┈┼┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┼┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┼┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┼┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┼┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┤";
+
   /**
    * Print header for table of records
-   *
    */
 
-  public static void printHeader(){
+  public static void printHeader() {
     System.out.println(Colors.WHITE_BOLD_BRIGHT + LINEOPEN + Colors.RESET);
     System.out.println(Colors.WHITE_BOLD_BRIGHT + HEADER + Colors.RESET);
     System.out.println(Colors.WHITE_BOLD_BRIGHT + Colors.RESET);
   }
+
   /**
    * make delay for 2000ms
-   *
    */
 
-  public static void delayMain(){
+  public static void delayMain() {
     int delay = 2000;
     long start = System.currentTimeMillis();
     while (start > System.currentTimeMillis() - delay)
+      ;
+  }
+
+  /**
+   * make delay for 100ms
+   */
+  public static void delaySecond() {
+    int delay = 100;
+    long start = System.currentTimeMillis();
+    while (start >= System.currentTimeMillis() - delay)
       ;
   }
 }
