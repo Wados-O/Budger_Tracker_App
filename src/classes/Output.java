a33496c930d1a9b836c369af18cd2cb0d4809393
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-07-23 11:36:27 +0200

    feat : add printList to users ;

--- src/classes/Output.java
+++ src/classes/Output.java
@@ -1,5 +1,28 @@
 package classes;
 
+import java.util.List;
+
 public class Output {
+
+  /**
+   * Print table with list of records
+   *
+   * @param records List of Record with payments (can be sorted and filtered before)
+   */
+  public static void printList(List<Record> records) {
+    for (Record record : records) {
+      String recordRow = String.format("│%6d│%24s│%25s│%22s│%24.2f│%34s│",
+          record.getId(), Operations.dateToString(record.getDate()), record.getCategory(),
+          record.getUser(), record.getAmount(), record.getComment());
+      System.out.println(recordRow);
+      System.out.println(
+          "├┈┈┈┈┈┈┼┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┼┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┼┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┼┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┼┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┤");
+    }
+    System.out.println(
+        "└┈┈┈┈┈┈┸┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┸┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┸┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┸┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┸┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┘");
+
+    // TODO
+  }
+
 
 }
