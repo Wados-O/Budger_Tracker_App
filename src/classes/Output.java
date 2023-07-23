0a8ec589fcb706b1b7e42613d4941c8907d2e729
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-07-23 17:19:56 +0200

    feat : add chartCategory to output;

--- src/classes/Output.java
+++ src/classes/Output.java
@@ -1,5 +1,6 @@
 package classes;
 
+import java.util.Date;
 import java.util.List;
 
 public class Output {
@@ -29,6 +30,30 @@ public class Output {
      * @param dateEnd    End of payments period
      */
 
+  }
+  public static void chartCategory(List<Record> records,List<Category> categories,
+      Date dateBegin,Date dateEnd){
+    String color = Colors.RESET;
+    System.out.println(Colors.WHITE_BRIGHT + "┎┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┒");
+    System.out.println(Colors.WHITE_BRIGHT +"┃     CATEGORY     ┃                                           AMOUNT                                          ┃");
+    System.out.println(Colors.WHITE_BRIGHT +"┖┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┸┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┚"+Colors.RESET);
+
+    double totalEx = -1 * Operations.calcExpensesPeriod(records,dateBegin,dateEnd);
+    for (Category category : categories){
+      double categorySum = -1 * Operations.expensesByCategory(records,category.getTitle(),dateBegin,dateEnd);
+      double percent = categorySum * 100 / totalEx;
+      System.out.printf("| %16s |",category.getTitle());
+      for (int i = 0; i < percent; i++) {
+        if (percent < 10) color = Colors.YELLOW;
+        if (percent > 10) color = Colors.BLUE;
+        if (percent > 15) color = Colors.CYAN;
+        if (percent > 30) color = Colors.RED_BRIGHT;
+        System.out.print(color + "*");
+        color = Colors.RESET;
+      }
+      percent = Math.round(percent);
+      System.out.println(Colors.WHITE_BRIGHT + " " + Colors.WHITE_BRIGHT + categorySum + Colors.RESET + " (" + percent + "% )");
+    }
   }
 
 
