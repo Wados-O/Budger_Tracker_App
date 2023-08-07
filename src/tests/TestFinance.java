85beb9326e3aa9ba66f1190034838d955dbb142f
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-08-07 03:48:42 +0200

    feat : add test checkExpensesByCategory

--- src/tests/TestFinance.java
+++ src/tests/TestFinance.java
@@ -71,4 +71,39 @@ public class TestFinance {
             dateFormat.parse("12.10.2023")));
 
   }
+
+  @Test
+  public void checkExpensesByCategory() throws ParseException {
+    List<Record> records = new ArrayList<>();
+    Record r1 = new Record(1, dateFormat.parse("10.10.2023"),
+        "mama", "Food", "Pizza1", -15.00);
+    Record r2 = new Record(2, dateFormat.parse("11.10.2023"),
+        "papa", "Goods", "Smartphone", -450.00);
+    Record r3 = new Record(3, dateFormat.parse("12.10.2023"),
+        "papa", "Transport", "Ticket to Berlin", -49.00);
+    Record r4 = new Record(4, dateFormat.parse("11.10.2023"),
+        "mama", "Income", "Salary", 1200.00);
+    Record r5 = new Record(1, dateFormat.parse("12.10.2023"),
+        "mama", "Food", "Pizza2", -15.00);
+    records.add(r1);
+    records.add(r2);
+    records.add(r3);
+    records.add(r4);
+    records.add(r5);
+
+    assertEquals(-30.00,
+        Operations.expensesByCategory(records, "Food", dateFormat.parse("09.10.2023"),
+            dateFormat.parse("13.10.2023")));
+    assertEquals(-15.00,
+        Operations.expensesByCategory(records, "Food", dateFormat.parse("09.10.2023"),
+            dateFormat.parse("12.10.2023")));
+    assertEquals(-49.00,
+        Operations.expensesByCategory(records, "Transport", dateFormat.parse("09.10.2023"),
+            dateFormat.parse("13.10.2023")));
+    assertEquals(-450.00,
+        Operations.expensesByCategory(records, "Goods", dateFormat.parse("09.10.2023"),
+            dateFormat.parse("13.10.2023")));
+  }
+
+
 }
