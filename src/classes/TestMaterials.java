a7161315b20ebfc320c6ba23da9fbb110b3c201f
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-08-13 03:53:11 +0200

    feat : add TEMPLATE CLASS

---
+++ src/classes/TestMaterials.java
@@ -0,0 +1,13 @@
+package classes;
+
+public class TestMaterials {
+
+  public static void main(String[] args) {
+    System.out.println(Menu.SHOW_SYSTEM_MENU_MAIN);
+    System.out.println(Menu.SHOW_ALL_MENU_MAIN);
+    System.out.println(Menu.SHOW_SYSTEM_MENU);
+    System.out.println(Menu.LEGEND);
+    System.out.println(Menu.SHOW_ALL_MENU_CHART_USER);
+    System.out.println(Menu.SHOW_SUB_MENU_CHART);
+  }
+}
